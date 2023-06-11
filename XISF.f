7797e520318e3a390322444cd11805eb8254a70c
Andrew Read <andrew81244@outlook.com>
2023-06-11 17:53:56 +0800

    Added string FITSkeys Added SHARED variables ImageWidth and ImageHeight

--- XISF.f
+++ XISF.f
@@ -1,8 +1,13 @@
-4080 constant XISFHeaderMaxLen			\ XISF_DATA will appear at 4096
+\ Forth language tools for creating PixInsight XISF image format
+\ requires ForthBase.f
+
+\ define the paramaters for the size of the in-memory buffer
+4080 constant XISFHeaderMaxLen			\ XISF_DATA will appear at offset 4096
 3096 constant XISFMaxImageWidth
 2080 constant XISFMaxImageHeight
 XISFMaxImageWidth XISFMaxImageHeight 2 * * constant XISFDataMaxLen
 
+\ XISF monolithic file structure, from the XISF specification
 BEGIN-STRUCTURE XISF_BUFFER
 	8 +FIELD XISF_SIGNATURE
 	4 +FIELD XISF_HEADER_LEN
@@ -11,59 +16,72 @@ BEGIN-STRUCTURE XISF_BUFFER
 	XISFDataMaxLen		+FIELD XISF_DATA 			\ data with trailing zeros
 END-STRUCTURE
 
-XISF_BUFFER allocate ( addr ior) throw constant XISFBuffer  \ BUFFER: cannot handle allocations of this size
-
-variable XISFBufferPointer
-variable XISFHeaderPointer
+variable XISFBufferPointer		\ address of the start of the presently active buffer
+variable XISFHeaderPointer		\ address of the current 'cursor location' in the presently active buffer
+variable XISFBufferSize			\ size of the presently active buffer
+SHARED variable ImageWidth		\ these variables will be set later by camera code
+SHARED variable ImageHeight
 	
-: XISF.StartHeader ( XISFbuff -- )
-	dup XISFBufferPointer !	
-	XISF_Header XISFHeaderPointer !
-;
-
 : XISF.HeaderLength ( -- n )
+\ compute the length in bytes of header as currently written
 	XISFHeaderPointer @ XISFBufferPointer @ XISF_Header -
 ;
-	
+
+: XISF.StartHeader ( XISFbuff -- )
+\ call this once to initialize the header
+	dup XISFBufferPointer !	
+	XISF_Header dup XISFHeaderPointer !
+	XISFHeaderMaxLen 0 ( addr n 0) fill		\ zero the header
+;
+
 : XISF.WriteToHeader ( addr n -- )
+\ write a string to the buffer at the current cursor location
 	dup >R
 	XISFHeaderPointer @ swap cmove
 	R> XISFHeaderPointer +!
 ;
 
 : XISF.WriteIntToHeader ( x -- )
+\ convert an integer to a string and it write to the header at the current cursor location
 	<# dup SIGN 0 ( x-as-double) #S #> ( caddr u)
 	XISF.WriteToHeader
 ;
-	
-: XISF.FinishHeader ( -- )
-	 s" XISF0100" XISFBufferPointer @ swap ( caddr buffer u ) cmove
-	 XISF.HeaderLength XISFBufferPointer @ XISF_HEADER_LEN ( len addr) l!
-;
 
 : XISF.StartXML ( -- )
+\ call this after XISF.StartHeader
 	s\" <?xml version=\"1.0\" encoding=\"UTF-8\"?>"	XISF.WriteToHeader
 	s\" <xisf version=\"1.0\">"		XISF.WriteToHeader
 ;
 
-: XISF.FinishXML
-	s\" </xisf>"							XISF.WriteToHeader
-;
-
 : XISF.StartImage
+\ call this after XISF.StartXML
 	s\" <Image geometry=\""				XISF.WriteToHeader
-	3096										XISF.WriteIntToHeader	\ width
+	ImageWidth @ dup						XISF.WriteIntToHeader	\ width
 	s\" :"									XISF.WriteToHeader
-	2080 	 									XISF.WriteIntToHeader	\ height
+	ImageHeight @ dup						XISF.WriteIntToHeader	\ height
 	s\" :1\" sampleFormat=\"UInt16\" colorSpace=\"Gray\" location=\"attachment:"	XISF.WriteToHeader
-	0 XISF_DATA 							XISF.WriteIntToHeader	\ location
+	0 XISF_DATA ( ... offset)			XISF.WriteIntToHeader	\ location
 	s\" :"									XISF.WriteToHeader
-	XISFDataMaxLen							XISF.WriteIntToHeader	\ size
+	( width height) 2 * * dup 			XISF.WriteIntToHeader	\ size in bytes of the image buffer
 	s\" \">"									XISF.WriteToHeader
+	( dataSize) 0 XISF_HEADER + XISFBufferSize !					\ update the buffer size to match the image size
 ;
 
 : XISF.FinishImage
+\ call this after XISF.StartImage and any optional FITS keywords
 	s\" </Image>"							XISF.WriteToHeader
+;
+
+: XISF.FinishXML
+\ call this after XISF.FinishImage
+	s\" </xisf>"							XISF.WriteToHeader
+;
+
+: XISF.FinishHeader ( -- )
+\ call this after XISF.FinishXML
+\ completes the header according to XISF specification
+	 s" XISF0100" XISFBufferPointer @ swap ( caddr buffer u ) cmove
+	 XISF.HeaderLength XISFBufferPointer @ XISF_HEADER_LEN ( len addr) l!
 ;
 
 : XISF.MAKE-FITSKEY-INT ( caddr u addr <name> -- ) 
@@ -72,18 +90,34 @@ variable XISFHeaderPointer
 	CREATE 
 		, $,
 	DOES> ( --)
-		dup >R @	@							( value)
+		dup >R @								( addr)
 		R> cell+ count						( value caddr u)
 		s\" <FITSKeyword name=\""		XISF.WriteToHeader
-		( value caddr u) 					XISF.WriteToHeader
+		( addr caddr u) 					XISF.WriteToHeader
 		s\" \" value=\""					XISF.WriteToHeader
-		( value) 							XISF.WriteIntToHeader
+		@ ( value) 							XISF.WriteIntToHeader
+		s\" \" />"							XISF.WriteToHeader
+;
+
+: XISF.MAKE-FITSKEY-STR ( caddr u addr <name> -- ) 
+\ defining word for a FITS key with a counted string
+\ e.g. string-name S" FITS-keyword" XISF.MAKE-FITSKEY-INT <name>
+	CREATE 
+		, $,
+	DOES> ( --)
+		dup >R @								( addr)
+		R> cell+ count						( value caddr u)
+		s\" <FITSKeyword name=\""		XISF.WriteToHeader
+		( addr caddr u) 					XISF.WriteToHeader
+		s\" \" value=\""					XISF.WriteToHeader
+		$@ ( value) 						XISF.WriteToHeader
 		s\" \" />"							XISF.WriteToHeader
 ;
 
 : XISF.WriteFile ( caddr n --)
+\ write the XISF buffer to an XISF file with the given (location and) name
 	w/o create-file if abort" Cannot create XISF file" then
-	>R XISFBufferPointer @ XISF_BUFFER R@	( caddr u fid)
+	>R XISFBufferPointer @ XISFBufferSize @ R@					( caddr u fid)
 	write-file if abort" Cannot write XISF file" then
 	R> close-file if abort" Cannot close XISF file" then
 ;
--- XISF_test1.f
+++ XISF_test1.f
@@ -1,19 +1,22 @@
 \ test for XISF.f
 
-XISF_BUFFER allocate ( addr ior) throw constant XISFBuffer  \ BUFFER: cannot handle allocations of this size
-variable fpos
-2000 fpos !
+XISF_BUFFER BUFFER XISFBuffer
+variable fpos		2000 fpos !
+256 buffer: instrument		s" Takahashi CCA-250" instrument $!
+640 ImageWidth !	480 ImageHeight !
 
 s" FOCUSPOS" fpos XISF.MAKE-FITSKEY-INT XISF.FITSfocuspos
+s" INSTRUMENT" instrument  XISF.MAKE-FITSKEY-STR XISF.FITSinstrument
 
 XISFBuffer XISF.StartHeader
-	XISF.StartXML
-		XISF.StartImage
-			XISF.FITSfocuspos
-		XISF.FinishImage
+	XISF.StartXML 
+	XISF.StartImage
+		XISF.FITSfocuspos
+		XISF.FITSinstrument
+	XISF.FinishImage 
 	XISF.FinishXML
 XISF.FinishHeader
 
-XISFBuffer 256 dump
+XISFBuffer 512 dump
 
 s" C:\test\MadeInForth.XISF" XISF.WriteFile

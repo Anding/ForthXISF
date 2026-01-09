af668b394e73af055073d681f4b9212f16e7f7ec
Andrew Read <andrew81244@outlook.com>
2026-01-09 12:40:34 +0800

    reformatting

--- XISF_maps.f
+++ XISF_maps.f
@@ -3,46 +3,48 @@
 
 need Windows
 
-UUIDlength buffer: UUIDstring  
-TSlength buffer: TSstring
+UUIDlength buffer: xisf.UUIDstring  
+TSlength buffer: xisf.TSstring
 
 
 \ the camera driver does not know - this must be set manually or by script
 0 value image_type
 
-: add-observationXISF ( map --)
-\ add key value pairs for XISF camera parameters
-	>R
- 	obs.type observationType	R@	=>" IMAGETYPE"
-   UUIDString zcount		 		R@ =>" UUID"	\ generated UUID			
-	R> drop
-;
+
 
 : add-observationFITS ( map --)
 \ add key value pairs for FITS observation parameters
 	>R
-	s"  " 										R@ =>" #OBS"			\ a header to indicate the source of these FITS values	
- 	obs.type observationType	R@ =>" IMAGETYP"	
+	s"  "                                   R@ =>" #OBS"            \ a header to indicate the source of these FITS values	
+ 	obs.type observationType                R@ =>" IMAGETYP"	
  	obs.type LIGHT = if obs.object else 0 0 then
- 														R@ =>" OBJECT"
-	TSstring 0 timestamp			R@ =>" DATE-OBS"		\ UTC date and time in ISO format
-	TSstring 1 timestamp			R@ =>" LOCAL-DT"		\ local date and time in ISO format
-	TSstring 3 timestamp drop 10	R@ =>" NIGHTOF"	\ local date in midday to midday format
- 	obs.observer							R@ =>" OBSERVER"			
- 	UUIDString make-UUID			R@ =>" UUID"			\ requires that add-observationXISF has been called first									
+                                            R@ =>" OBJECT"
+	xisf.TSstring 0 make-timestamp          R@ =>" DATE-OBS"        \ UTC date and time in ISO format
+	xisf.TSstring 1 make-timestamp          R@ =>" LOCAL-DT"        \ local date and time in ISO format
+	xisf.TSstring 3 make-timestamp drop 10  R@ =>" NIGHTOF"         \ local date in midday to midday format
+ 	obs.observer                            R@ =>" OBSERVER"			
+ 	xisf.UUIDString make-UUID               R@ =>" UUID"											
 	R> drop
 ;	
 
 : add-rigFITS ( map --)
 \ add key value pairs for FITS rig parameters
 	>R
-	s"  " 									R@ =>" #RIG"			\ a header to indicate the source of these FITS values	
-	rig.telescope						R@ =>" TELESCOP"
-	rig.focal_len 					R@ =>" FOCALLEN"	
-	rig.focal_ratio					R@ =>" FOCRATIO"	
-	rig.aperature_dia 			R@ =>" APTDIA"			
-	rig.aperature_area 			R@ =>" APTAREA"
-	rig.software						R@ =>" SWCREATE"		
+	s"  "                                   R@ =>" #RIG"            \ a header to indicate the source of these FITS values	
+	rig.telescope                           R@ =>" TELESCOP"
+	rig.focal_len                           R@ =>" FOCALLEN"	
+	rig.focal_ratio                         R@ =>" FOCRATIO"	
+	rig.aperature_dia                       R@ =>" APTDIA"			
+	rig.aperature_area                      R@ =>" APTAREA"
+	rig.software                            R@ =>" SWCREATE"		
 	R> drop
 ;	
+
+: add-observationXISF ( map --)
+\ add key value pairs for XISF camera parameters
+	>R
+ 	obs.type observationType                R@ =>" IMAGETYPE"
+    xiaf.UUIDString UUIDlength              R@ =>" UUID"            \ requires that add-observationFITS has been called first			
+	R> drop
+;
 

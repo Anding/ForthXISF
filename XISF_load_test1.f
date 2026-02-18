76a655946e5f7b87476c11db763662750117585a
Andrew Read <andrew81244@outlook.com>
2026-02-18 15:31:06 +0800

    update test for XISF-load

--- XISF_load_test1.f
+++ XISF_load_test1.f
@@ -1,5 +1,32 @@
 need forthXISF
 need simple-tester
+0 value image1
+0 value image2
+
+: testA_write-XISFfilepath ( map buf -- )
+	>R drop
+	s" e:\coding\ForthXISF\testdata\" R@ write-buffer drop
+	R@ buffer-punctuate-filepath
+	s" test2.xisf" R@ write-buffer drop
+	R> drop
+;
+	ASSIGN testA_write-XISFfilepath TO-DO write-XISFfilepath
+	
+: make-test2.xisf { | map img -- img }
+    640 480 1 allocate-image -> img
+    img FITS_MAP @ -> map
+    s" 16" map =>" BITPIX"	
+    s" 2"	map =>" NAXIS"	
+    s" 640" map =>" NAXIS1"
+    s" 480" map =>" NAXIS2" 
+    640 480 * 0 do
+        0x10000 choose img IMAGE_BITMAP i 2* + w!   \ random 16 bit words
+    loop   
+    img save-XISFimage 
+    img
+;
+
+    make-test2.xisf -> image1
 
 : test_write-XISFfilepath { map buf -- }
 	s" E:\coding\ForthXISF\testdata\" buf write-buffer drop
@@ -9,19 +36,24 @@ need simple-tester
 
 ASSIGN test_write-XISFfilepath TO-DO write-XISFfilepath
 
-: test.xisf.load-file ( -- )
-    s" E:\coding\ForthXISF\testdata\LUM-E155-F5100-f7843758a3f5.xisf" xisf.load-file ( img 0 | IOR )
-    if 
-        abort" xisf.load-file failed" 
+: test.xisf.load-file  ( caddr u -- img)
+    xisf.load-file ( img 0 | IOR )
+    0= if 
+        dup save-XISFimage
     else 
-        save-XISFimage 
+        0
     then
 ;         
  
 cr 
 Tstart
-T{ test.xisf.load-file }T ==
-T{ s" E:\coding\ForthXISF\testdata\clone.xisf" hashF }T s" E:\coding\ForthXISF\testdata\LUM-E155-F5100-f7843758a3f5.xisf" hashF ==
+
+T{ s" E:\coding\ForthXISF\testdata\test2.xisf" test.xisf.load-file -> image2
+   s" E:\coding\ForthXISF\testdata\clone.xisf" hashF }T 
+   s" E:\coding\ForthXISF\testdata\test2.xisf" hashF ==
+   
+cr
 Tend
+cr
 
     
